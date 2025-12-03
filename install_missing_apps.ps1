dc145d6e75816f8235cd8cabd6bd31e1eb73d176
Aidrak Test <test@aidrak.dev>
2025-12-03 05:49:47 +0000

    Dec 3, 2025, 5:49 AM

---
+++ install_missing_apps.ps1
@@ -0,0 +1,34 @@
+Write-Output "--- Retrying App Installation ---"
+
+# Refresh env vars just in case
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+Write-Output "Installing Adobe Reader..."
+choco install adobereader -y --verbose
+
+Write-Output "Installing Google Chrome..."
+choco install googlechrome -y --verbose
+
+# Retry Shortcuts
+$publicDesktop = "C:\Users\Public\Desktop"
+$shell = New-Object -ComObject WScript.Shell
+
+function Create-Shortcut {
+    param($Path, $Target, $Desc)
+    if (Test-Path $Target) {
+        $s = $shell.CreateShortcut($Path)
+        $s.TargetPath = $Target
+        $s.Description = $Desc
+        $s.Save()
+        Write-Output "SUCCESS: Created $Path"
+    } else {
+        Write-Output "ERROR: Target still not found: $Target"
+    }
+}
+
+Write-Output "--- Re-creating Shortcuts ---"
+Create-Shortcut "$publicDesktop\Adobe Acrobat Reader.lnk" "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe" "Adobe Acrobat Reader DC"
+Create-Shortcut "$publicDesktop\Google Chrome.lnk" "C:\Program Files\Google\Chrome\Application\chrome.exe" "Google Chrome"
+
+# Set Default PDF
+cmd /c assoc .pdf=AcroExch.Document.DC
